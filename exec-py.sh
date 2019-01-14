4366cf7736a866df47bab2d7008f422ea1fa70f1
Xavier Martorell <xavim@ac.upc.edu>
2019-01-14 18:31:36 +0100

    python test

---
+++ exec-py.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LD_PRELOAD=/home/xavim/Downloads/ompss/install/lib/performance/libnanox-ompss.so:/home/xavim/Downloads/ompss/install/lib/performance/libnanox-c.so:/home/xavim/Downloads/ompss/install/lib/performance/libnanox-pe-smp.so python3 darknet.py
